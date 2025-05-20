a967fba5860aec02f19b44088bf1437d6bf1e552
SantiGiusiano1 <santyg152@gmail.com>
2025-05-19 23:08:13 -0300

    Consultas lista

--- consultas_sql/consultas.sql
+++ consultas_sql/consultas.sql
@@ -1,33 +1,26 @@
 SELECT
-    registro_seccional.descripcion AS registro_seccional_descripcion,
-    automotor.automotor_anio_modelo,
+    transferencia.registro_seccional_descripcion AS registro_seccional_descripcion,
+    transferencia.automotor_anio_modelo,
     COUNT(*) AS cantidad
-FROM
-    transferencia
-    INNER JOIN automotor ON transferencia.automotor_id = automotor.id
-    INNER JOIN registro_seccional ON automotor.registro_seccional_id = registro_seccional.id
+FROM transferencia
 GROUP BY
-    registro_seccional.descripcion,
-    automotor.automotor_anio_modelo;
+    transferencia.registro_seccional_descripcion,
+    transferencia.automotor_anio_modelo;
 
 SELECT
-    tipo_automotor.codigo AS automotor_tipo_codigo,
-    tipo_automotor.descripcion AS automotor_tipo_descripcion,
-    automotor.modelo_descripcion AS automotor_modelo_descripcion,
+    transferencia.automotor_tipo_codigo AS automotor_tipo_codigo,
+    transferencia.automotor_tipo_descripcion AS automotor_tipo_descripcion,
+    transferencia.automotor_modelo_descripcion AS automotor_modelo_descripcion,
     COUNT(*) AS cantidad
-FROM
-    transferencia
-    INNER JOIN automotor ON transferencia.automotor_id = automotor.id
-    INNER JOIN tipo_automotor ON automotor.tipo_automotor_id = tipo_automotor.id
-GROUP BY
-    tipo_automotor.codigo,
-    tipo_automotor.descripcion,
-    automotor.modelo_descripcion;
-
-SELECT automotor.tramite_fecha, automotor.automotor_origen, COUNT(*) AS cantidad
 FROM transferencia
-    INNER JOIN automotor ON transferencia.automotor_id = automotor.id
 GROUP BY
-    automotor.tramite_fecha,
-    automotor.automotor_origen
-ORDER BY automotor.tramite_fecha DESC;
+    transferencia.automotor_tipo_codigo,
+    transferencia.automotor_tipo_descripcion,
+    transferencia.automotor_modelo_descripcion;
+
+SELECT transferencia.tramite_fecha, transferencia.automotor_origen, COUNT(*) AS cantidad
+FROM transferencia
+GROUP BY
+    transferencia.tramite_fecha,
+    transferencia.automotor_origen
+ORDER BY transferencia.tramite_fecha DESC;
